338eb89e1f3fe5e5fcea42e50ffe81e78e4a17df
isabelle9912 <isabellesaahirah28@gmail.com>
2025-02-03 12:11:33 -0300

    fix: variáveis de ambiente na conexão com o banco

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const dbHost = process.env.PGHOST;
-const dbName = process.env.PGDATABASE;
-const dbUser = process.env.PGUSER;
-const dbPassword = process.env.PGPASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 const { Sequelize } = require("sequelize");
 
